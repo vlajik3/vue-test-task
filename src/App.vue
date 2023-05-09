e448afeea664c26f7069f0eb14e81a39b772dcc7
Tselishchev Vladislav <celishchevva@yandex.ru>
2023-05-09 21:11:46 +0300

    [changed] - styles

--- src/App.vue
+++ src/App.vue
@@ -54,20 +54,19 @@ export default defineComponent({
 </script>
 
 <style>
-#app {
+* {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
 }
 .charts {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 0 80px;
+  padding: 0 20px;
 }
 .chart-item {
   width: 100%;
